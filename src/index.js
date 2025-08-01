2b45d7bc2d3bbac0d0dbb3240645947136f987ad
JITENDRA SINGH BISHT <152790288+vickybisht001@users.noreply.github.com>
2025-08-01 11:21:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,15 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-window.addEventListener("scroll", function () {
-  let navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("sticky", window.scrollY > 0);
-  navbar.classList.toggle("bg-black",window.scrollY>0);
-});
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
