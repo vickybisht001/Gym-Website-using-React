7e73ab49e66b81792eecb277847526da73540422
JITENDRA SINGH BISHT <152790288+vickybisht001@users.noreply.github.com>
2025-08-01 11:35:42 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,84 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg fixed-top">
-          <div className="container-fluid">
-            <NavLink className="navbar-brand ms-3" to="/"><img src="https://preview.colorlib.com/theme/gymlife/img/logo.png.webp" alt="" /></NavLink>
-            <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/about">About Us</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/class">Classes</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/service">Services</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/team">Our Team</NavLink>
-                </li>
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-                <li className="nav-item">
-                  <NavLink className={(e) => { return e.isActive ? "active-col" : "" }} to="/contact">Contact</NavLink>
-                </li>
+  const handleNavClick = (path) => {
+    navigate(path);
+    setIsOpen(false);
+  };
 
-              </ul>
-              <div className='nav-media'>
-                <a href="/"><i className='bx bxl-facebook'></i></a>
-                <a href="/"><i className='bx bxl-twitter'></i></a>
-                <a href="/"><i className='bx bxl-youtube'></i></a>
-                <a href="/"><i className='bx bxl-instagram'></i></a>
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top bg-black">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand ms-3" to="/">
+          <img
+            src="https://preview.colorlib.com/theme/gymlife/img/logo.png.webp"
+            alt="Gym Life Logo"
+          />
+        </NavLink>
+        <button
+          className="navbar-toggler text-white"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls="navbarNav"
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation"
+        >
+          {isOpen ? (
+            <i className="bx bx-x fs-3"></i>
+          ) : (
+            <i className="bx bx-menu fs-3"></i>
+          )}
+        </button>
 
-              </div>
+        <div
+          className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            {[
+              { name: "Home", path: "/" },
+              { name: "About", path: "/about" },
+              { name: "Classes", path: "/class" },
+              { name: "Service", path: "/service" },
+              { name: "Team", path: "/team" },
+              { name: "Contact", path: "/contact" },
+            ].map((link) => (
+              <li className="nav-item" key={link.path}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `nav-link ${isActive ? "active-col" : ""}`
+                  }
+                  to={link.path}
+                  onClick={() => handleNavClick(link.path)}
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
 
-            </div>
+          <div className="nav-media d-none d-lg-flex ms-3">
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              <i className="bx bxl-facebook"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              <i className="bx bxl-twitter"></i>
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noreferrer">
+              <i className="bx bxl-youtube"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              <i className="bx bxl-instagram"></i>
+            </a>
           </div>
-        </nav>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-
-      </>
-    )
-  }
-}
-
-export default Navbar
+export default Navbar;
